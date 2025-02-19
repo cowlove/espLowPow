42cde57b3df6117cf5e2f3c82d066cab90cc0121
jim@vheavy.com <jim@vheavy.com>
2025-02-19 12:14:29 -0800

    Fix typo

--- espLowPow.ino
+++ espLowPow.ino
@@ -90,7 +90,7 @@ void loop() {
 	
 		String spost = 
 			Sfmt("{\"PROGRAM\":\"%s\",", basename_strip_ext(__BASE_FILE__).c_str()) + 
-			Sfmt("{\"GIT_VERSION\":\"%s\",", GIT_VERSION) + 
+			Sfmt("\"GIT_VERSION\":\"%s\",", GIT_VERSION) + 
 			Sfmt("\"MAC\":\"%s\",", mac.c_str()) + 
 			Sfmt("\"IP\":\"%s\",", WiFi.localIP().toString().c_str()) + 
 			Sfmt("\"RSSI\":%d,", WiFi.RSSI()) +
