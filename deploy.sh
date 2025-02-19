18983c591216c26488a643ff717a6282bca26ad5
jim@vheavy.com <jim@vheavy.com>
2025-02-19 11:29:58 -0800

    Changes to try and make ota work

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 touch *.ino
 make
 ssh ls 'rm simplePost/firmware.bin simplePost/firmware.ver'
-strings /tmp/mkESP/espLowPow_esp32doit-devkit-v1/espLowPow_.cpp.o | grep GIT_VER |
-     rematch '"([0-9a-fA-F]+)"' > firmware.ver
+#strings /tmp/mkESP/espLowPow_esp32doit-devkit-v1/espLowPow_.cpp.o | grep GIT_VER |
+#     rematch '"([0-9a-fA-F]+)"' > firmware.ver
+git describe --abbrev=8 --dirty --always --tags > firmware.ver
 scp /tmp/mkESP/espLowPow_esp32doit-devkit-v1/espLowPow.bin ls:simplePost/firmware.bin
 scp firmware.ver ls:simplePost/firmware.ver
     
