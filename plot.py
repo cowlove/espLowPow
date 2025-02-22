32a0338aacf16265ca6e8a02581b6dc386e455b5
jim@vheavy.com <jim@vheavy.com>
2025-02-22 15:22:59 -0800

    Add -q flag to plot.py

--- plot.py
+++ plot.py
@@ -3,8 +3,8 @@
 import os
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--metar", help="fetch and update metar data",
-                    action="store_true")
+parser.add_argument("-m", "--metar", help="fetch and update metar data", action="store_true")
+parser.add_argument("-q", "--quick", help="don't fetch any external data, just plot", action="store_true")
 args = parser.parse_args()
 
 
@@ -14,10 +14,11 @@ if args.metar:
     os.system("cat ./data/metar-* | ./metar_parser.py | sort -n | uniq > ./data/metar.dat")
     print("OK")
 
-print("Fetching posted data...",end='', flush=True)
-os.system("ssh ls grep BB0 simplePost/screenlog.0 | grep MOF-Guest > ./data/plane_data.txt")
-os.system("cat ./data/plane_data.txt | ./metar_parser.py > ./data/plane.dat")
-print("OK")
+if not args.quick:
+    print("Fetching posted data...",end='', flush=True)
+    os.system("ssh ls grep BB0 simplePost/screenlog.0 | grep MOF-Guest > ./data/plane_data.txt")
+    os.system("cat ./data/plane_data.txt | ./metar_parser.py > ./data/plane.dat")
+    print("OK")
 
 os.system('gnuplot -e "set terminal qt size 1200,800;p [483360:*][-1:15] \'./data/plane.dat\' u 1:3 w l, \'./data/metar.dat\' u 1:3 w l; pause 1111"')
 
